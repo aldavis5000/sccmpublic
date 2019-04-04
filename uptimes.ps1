081dfa60829a4b9bc0cfc731701cb2111ace9688
aldavis5000 <49279752+aldavis5000@users.noreply.github.com>
2019-04-04 09:13:21 -0500

    cleanup

--- uptimes.ps1
+++
@@ -1,9 +0,0 @@
-﻿$Computername = Get-Content C:\myscripts\computers.txt
-foreach ($Computer in $ComputerName) {
-        
-        New-Object psobject -Property @{
-            ComputerName = $Computer
-            LastBootUpTime = [Management.ManagementDateTimeConverter]::ToDateTime( (Get-WmiObject -Class Win32_OperatingSystem -Computer $Computer | Select -Exp LastBootUpTime) )
-        }
-    
-    }
